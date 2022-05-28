6456ff971a8dcecf76a4ea5218a9113721c9665d
Leonardo Custodio <44417666+leoskiline@users.noreply.github.com>
2022-05-27 22:26:36 -0300

    Melhorias

--- docs/js/website.js
+++ docs/js/website.js
@@ -6,3 +6,33 @@ function collapse() {
     x.className = "menu";
   }
 }
+
+function hideButton()
+{
+  document.getElementById("btnScrollUp").classList.remove("d-block")
+  document.getElementById("btnScrollUp").classList.add("d-none")
+}
+
+function showHideButton()
+{
+  if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) 
+  { 
+    document.getElementById("btnScrollUp").classList.remove("d-none")
+    document.getElementById("btnScrollUp").classList.add("d-block")
+  } 
+  else { 
+    document.getElementById("btnScrollUp").classList.remove("d-block")
+    document.getElementById("btnScrollUp").classList.add("d-none")
+  }
+}
+
+var mousewheelevt=(/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+
+if (document.attachEvent) //if IE (and Opera depending on user setting)
+    document.attachEvent("on"+mousewheelevt, function(e){
+      showHideButton();
+    })
+else if (document.addEventListener) //WC3 browsers
+    document.addEventListener(mousewheelevt, function(e){
+      showHideButton();
+    }, false)
